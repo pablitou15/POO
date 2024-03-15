6d62bd96a3e4ea19ddb47a16906b43fd4bcb4cc1
reyesmario213 <marioreyes@liceovvh.cl>
2024-03-15 12:20:14 -0300

    ok

---
+++ script.js
@@ -0,0 +1,32 @@
+var global ="Global";
+
+let local = "Local";
+
+const constante = "constante";
+
+console.log("Variables: Global"+ global + "- Local: " + local + " - Constante: " + constante);
+
+let num = 4;
+let decimal = 3.1416;
+let string = "texto"
+let bool = true;
+let array = ["perro",20, "Gato", 50];
+let objeto = {
+    Edad:20, 
+    Nombre: "Dany",
+    Telefono: "8116851",
+    Direccion: "El Rosal 2158"
+};
+let indefinida;
+let nula = null;
+
+function sumar(num1, num2){
+return num1 + num2;
+};
+console.log(
+    sumar(10,5)
+);
+
+
+
+console.log("variables:\nNum: " + num + " \nDecimal: " + decimal + "\nString: " + String + "\nBool: " + bool + "\nArray: " + Array + "\nObjeto: " + JSON.stringify(objeto) + "\nIndefinida: " + indefinida + "\nNula: " + nula + "\nResultado de funcion: " + sumar());
