759b6fda103d97fb18b264b78ad47795dcdf3546
pablitou15 <pablofernandez@liceovvh.cl>
2024-04-17 12:59:14 -0400

    ok

---
+++ Ejercicio_for/script.js
@@ -0,0 +1,11 @@
+function numeroMayor(){
+  let arrayNum = [];
+  let numMayor = 0;
+
+  for(let i = 0; i < 5; i++){
+      let num = parseInt(prompt("Ingrese un numero para determinar Mayor"));
+    arrayNum.push(num);}
+    if(numMayor > num){
+        numMayor = num;
+    }
+}
